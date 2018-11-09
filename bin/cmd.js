d7b6db15b10a02a7968d1288fd2aacc121cccc93
sethvincent <sethvincent@gmail.com>
2018-11-09 12:05:59 -0800

    use eslint-config-devseed-standard

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
 if (process.version.match(/v(\d+)\./)[1] < 4) {
-  console.error('standard: Node v4 or greater is required. `devseed-standard` did not run.');
+  console.error('devseed-standard: Node v4 or greater is required. `devseed-standard` did not run.')
 } else {
-  const opts = require('../options');
-  require('standard-engine').cli(opts);
+  const opts = require('../options')
+  require('standard-engine').cli(opts)
 }
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const Linter = require('standard-engine').linter;
-const opts = require('./options');
+const Linter = require('standard-engine').linter
+const opts = require('./options')
 
-module.exports = new Linter(opts);
+module.exports = new Linter(opts)
